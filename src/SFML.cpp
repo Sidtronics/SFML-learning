95b8ff501beffdf1a923e6416635bc3db840f6c1
Siddhesh-arch <siddheshdharme18@gmail.com>
2022-11-26 20:58:01 +0530

    Added graphics

--- src/SFML.cpp
+++ src/SFML.cpp
@@ -1,9 +1,84 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <sstream>
+#include <thread>
+
+void renderingThread(sf::RenderWindow *window, bool &closingWindow) {
+
+    window->setActive(true);
+
+    sf::Texture marbleTex;
+    marbleTex.loadFromFile("marble.png");
+    marbleTex.setSmooth(true);
+
+    sf::Sprite marbleSpr;
+    marbleSpr.setTexture(marbleTex);
+    marbleSpr.setOrigin(sf::Vector2f(300.f , 300.f));
+    marbleSpr.setPosition(400,300);
+
+    sf::Font font;
+    font.loadFromFile("fira-code.ttf");
+    sf::Text info, help;
+    info.setFont(font);
+    info.setCharacterSize(15);
+    info.setFillColor(sf::Color::White);
+
+    help.setFont(font);
+    help.setCharacterSize(15);
+    help.setFillColor(sf::Color::White);
+    help.setString("Use arrow keys to move, + - to resize, 1 2 3 to change colour.");
+    help.setPosition(5, 580);
+
+    u_char R=0,G=0,B=0;
+    float scale = 1.f;
+    while(!closingWindow) {
+        
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            marbleSpr.move(0,-1);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            marbleSpr.move(0,1);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            marbleSpr.move(1,0);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            marbleSpr.move(-1,0);
+
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+            R++;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+            G++;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+            B++;
+        marbleSpr.setColor(sf::Color(R,G,B));
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Add))
+            scale += 0.01f;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract))
+            scale -= 0.01f;
+        marbleSpr.setScale(scale,scale);
+
+        std::stringstream info_ss;
+        info_ss << "R: " << +R << "\n" << "G: " 
+                << +G << "\n" << "B: " << +B << "\n" << "scale: " << scale;
+        info.setString(info_ss.str());
+
+        window->clear(sf::Color::Black);
+        window->draw(info);
+        window->draw(help);
+        window->draw(marbleSpr);
+        window->display();
+    }
+
+}
 
 int main() {
 
-    sf::Window window(sf::VideoMode(800,600), "Arch-SFML");
+    sf::RenderWindow window(sf::VideoMode(800,600), "Arch-SFML");
+    window.setActive(false);
+
+    bool closingWindow = false;
+    std::thread Renderer(renderingThread, &window, std::ref(closingWindow));
 
     while(window.isOpen()) {
     
@@ -13,13 +88,18 @@ int main() {
             switch(event.type) {
 
                 case sf::Event::Closed:
+                    closingWindow = true;
+                    Renderer.join();
                     window.close();
                     break;
 
                 case sf::Event::KeyPressed:
                     
                     if(event.key.code == sf::Keyboard::Escape) {
-                        std::cout << "Close Requested...closing\n";
+
+                        std::cout << "Closing through ESC...\n";
+                        closingWindow = true;
+                        Renderer.join();
                         window.close();
                     }
                     break;
